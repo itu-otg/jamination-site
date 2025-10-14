7d3cf5d754b38c819905d5b91e0f8b3ebb90049a
Kürşat "SessizLeylek" Kuyumcu <kursatkymcu@gmail.com>
2025-10-14 22:57:33 +0300

    Remove apply button if application time is over

--- assets/shared/script.js
+++ assets/shared/script.js
@@ -2,7 +2,7 @@ const applyButton = document.querySelector(".nav-buttons .growing-button");
 const applicationDeadline = "2025-10-13T00:00:00.000+03:00";
 
 if (new Date() > new Date(applicationDeadline)) {
-    applyButton.hidden = false;
+    applyButton.remove();
     //applyButton.removeAttribute("href");
     //applyButton.textContent = "Başvuru süresi doldu!";
 }
