a45c81d590b935b9803b8511d4b3b01002cba049
Kürşat "SessizLeylek" Kuyumcu <kursatkymcu@gmail.com>
2025-10-14 22:49:44 +0300

    update some buttons

--- docs/shared/script.js
+++ docs/shared/script.js
@@ -2,6 +2,7 @@ const applyButton = document.querySelector(".nav-buttons .growing-button");
 const applicationDeadline = "2025-10-13T00:00:00.000+03:00";
 
 if (new Date() > new Date(applicationDeadline)) {
-    applyButton.removeAttribute("href");
-    applyButton.textContent = "Başvuru süresi doldu!";
+    applyButton.hidden = false;
+    //applyButton.removeAttribute("href");
+    //applyButton.textContent = "Başvuru süresi doldu!";
 }
